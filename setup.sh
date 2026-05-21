55a03a917e3ff33c95e27a64f9d55daf4856b540
Yassine Safraoui <yassine2safraoui@gmail.com>
2026-05-21 18:14:22 +0100

    chore: add setup.sh for Android SDK/NDK environment variables

---
+++ setup.sh
@@ -0,0 +1,4 @@
+export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_SDK_HOME=~/Library/Android/sdk
+export ANDROID_NDK_HOME=~/Library/Android/sdk/ndk/30.0.14904198
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools
